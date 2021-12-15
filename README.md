6e31d93413894cde01ef152f15ca6ab1b24b8baa
Hide <padocon@naver.com>
2021-12-15 23:31:05 +0900

    Version 0.0.1

---
+++ README.md
@@ -0,0 +1,16 @@
+# fastapi-event
+[![license]](/LICENSE)
+[![pypi]](https://pypi.org/project/fastapi-event/)
+[![pyversions]](http://pypi.python.org/pypi/fastapi-event)
+![badge](https://action-badges.now.sh/teamhide/fastapi-event)
+[![Downloads](https://pepy.tech/badge/pythondi)](https://pepy.tech/project/fastapi-event)
+
+---
+
+fastapi-event is event dispatcher for FastAPI framework.
+
+## Installation
+
+```python
+pip3 install fastapi-event
+```
--- setup.py
+++ setup.py
@@ -5,25 +5,28 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="fastapi-event-publisher",
-    version="1.2.2",
+    name="fastapi-event",
+    version="0.0.1",
     author="Hide",
     author_email="padocon@naver.com",
-    description="Python dependency injection library",
+    description="Event dispatcher for FastAPI",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/teamhide/pythondi",
+    url="https://github.com/teamhide/fastapi-event",
     packages=setuptools.find_packages(),
+    install_requires=[
+        "fastapi",
+        "pydantic",
+    ],
     tests_require=['pytest'],
     classifiers=[
         "Intended Audience :: Developers",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.5',
+    python_requires='>=3.7',
 )
