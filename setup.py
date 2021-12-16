a573d66e26e901ac2bc9dc8c9e95e6e59315c9e1
Hide <padocon@naver.com>
2021-12-16 23:47:27 +0900

    Version 0.0.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fastapi-event",
-    version="0.0.3",
+    version="0.0.4",
     author="Hide",
     author_email="padocon@naver.com",
     description="Event dispatcher for FastAPI",
