cb6ec0f66b4949adcdee21bc0a14e321f1551aab
Hide <padocon@naver.com>
2022-02-09 12:50:54 +0900

    Version 0.1.0
    - Fix ordering for event

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fastapi-event",
-    version="0.0.9",
+    version="0.1.0",
     author="Hide",
     author_email="padocon@naver.com",
     description="Event dispatcher for FastAPI",
